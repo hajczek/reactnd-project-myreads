e82fca708ae1933e60202c6c27f2cfb8102cce3e
hajczek <i.hajczewska@gmail.com>
2018-05-03 09:23:51 +0200

    feat: Add PropTypes

--- src/SearchView.js
+++ src/SearchView.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import SelectCategory from './SelectCategory'
 
 function SearchView (props) {
--- src/SelectCategory.js
+++ src/SelectCategory.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 function SelectCategory (props) {
     return (
