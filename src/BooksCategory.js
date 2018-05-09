e2e589a90cccc8d3d47d20afe7bec77401f49e34
hajczek <i.hajczewska@gmail.com>
2018-05-09 16:20:10 +0200

    fix: changed class for function

--- src/BooksCategory.js
+++ src/BooksCategory.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react'
-/* import PropTypes from 'prop-types' */
 import Book from './Book'
 
-class BooksCategory extends Component {
-
-    render() {        
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.categoryName}</h2>
-                <div className="bookshelf-books">
-                <ol className="books-grid">
-                    {this.props.books.map((book) => (
-                    <li key={book.id}>                        
-                        <Book 
-                            books = {this.props.books}
-                            onChangeCategory={this.props.onChangeCategory}
-                            book = {book}
-                        />
-                    </li>
-                    ))}
-                </ol>              
-            </div>
-        </div>  
-        )
-    }
+function BookCategory (props) {
+    return(
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{props.categoryName}</h2>
+            <div className="bookshelf-books">
+            <ol className="books-grid">
+                {props.books.map((book) => (
+                <li key={book.id}>                        
+                    <Book 
+                        books = {props.books}
+                        onChangeCategory={props.onChangeCategory}
+                        book = {book}
+                    />
+                </li>
+                ))}
+            </ol>              
+        </div>
+    </div>  
+   )
 }
 
-export default BooksCategory
+export default BookCategory
